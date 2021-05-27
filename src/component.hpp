0c06cfb7d629b494e66fd5033cf2a12443eada48
JeffTheK <jeff144@protonmail.com>
2021-05-27 20:10:26 +0300

    Add component class

---
+++ src/component.hpp
@@ -0,0 +1,13 @@
+#include "id.hpp"
+
+template<typename DERIVED>
+class Component {
+public:
+    Component() {
+        class_id = get_type_id<DERIVED>();
+    }
+
+    id_t get_class_id() const noexcept { return class_id; }
+private:
+	id_t class_id = 0;
+};
