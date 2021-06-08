e433ca8d42358362ffb3f3309110df6b07a2eaad
JeffTheK <jeff144@protonmail.com>
2021-06-08 13:15:20 +0300

    Implement get_input()

---
+++ src/input.cpp
@@ -0,0 +1,7 @@
+#include "input.hpp"
+
+std::string get_input() {
+    std::string str;
+    std::cin >> str;
+    return str;
+}
---
+++ src/input.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+
+static std::string get_input();
