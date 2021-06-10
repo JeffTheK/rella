c73ea93ba31bd3f2e03f32e092146e4a6d92c1ff
JeffTheK <jeff144@protonmail.com>
2021-06-10 13:34:42 +0300

    Test load_random_names()

--- test/name_test.cpp
+++ test/name_test.cpp
@@ -13,3 +13,7 @@ TEST_CASE("load_from_json") {
     auto comp = Name::load_from_json(comp_json);
     CHECK(comp.name == "foo");
 }
+
+TEST_CASE("load_random_names") {
+    Name::load_random_names();
+}
