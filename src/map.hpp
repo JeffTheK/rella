10502dec541e5ab9ede3506f5383a1e44d19f7fc
JeffTheK <jeff144@protonmail.com>
2021-06-24 14:30:23 +0300

    Put map size in namespace

--- src/map.hpp
+++ src/map.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
-const static unsigned int size_x = 100;
-const static unsigned int size_y = 100;
+namespace map {
+    const static unsigned int size_x = 100;
+    const static unsigned int size_y = 100;
+}
 
 void generate_map();
