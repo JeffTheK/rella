dc54f8417d1fbe4fdd90b20c7523e1eb15849f25
JeffTheK <jeff144@protonmail.com>
2021-06-26 00:27:31 +0300

    Add north to init_commands

--- src/command.cpp
+++ src/command.cpp
@@ -1,5 +1,8 @@
 #include "command.hpp"
 #include "loop.hpp"
+#include "entity.hpp"
+#include "player.hpp"
+#include "position.hpp"
 
 Command::Command(std::string key_, std::function<bool()> function_) {
     key = key_;
@@ -16,6 +19,14 @@ void init_commands() {
         [](){
             game_is_running = false;
             return false;
+        }
+    });
+    add_command({ // ^
+        "north",
+        [](){
+            auto pos = player_entity->get_component<Position>();
+            pos->y++;
+            return true;
         }
     });
 }
