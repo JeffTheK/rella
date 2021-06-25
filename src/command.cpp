5b9514cbb398bf613be1bdd17cd5e103aae34e78
JeffTheK <jeff144@protonmail.com>
2021-06-26 00:55:44 +0300

    Move the rest of commands into init_commands()

--- src/command.cpp
+++ src/command.cpp
@@ -3,6 +3,8 @@
 #include "entity.hpp"
 #include "player.hpp"
 #include "position.hpp"
+#include "name.hpp"
+#include <iostream>
 
 Command::Command(std::vector<std::string> keys_, std::function<bool()> function_) {
     keys = keys_;
@@ -28,5 +30,53 @@ void init_commands() {
             pos->y++;
             return true;
         }
+    });
+    add_command({
+        {"east", "e", "l"},
+        [](){
+            auto pos = player_entity->get_component<Position>();
+            pos->x++;
+            return true;
+        }
+    });
+    add_command({
+        {"south", "s", "j"},
+        [](){
+            auto pos = player_entity->get_component<Position>();
+            pos->y--;
+            return true;
+        }
+    });
+    add_command({
+        {"west", "w", "h"},
+        [](){
+            auto pos = player_entity->get_component<Position>();
+            pos->x--;
+            return true;
+        }
+    });
+    add_command({
+        {"look"},
+        [](){
+            auto pos = player_entity->get_component<Position>();
+            auto func = [pos](Entity& e) -> bool { 
+                auto other_pos = *e.get_component<Position>();
+                return other_pos == (*pos);
+            };
+            auto entities = find_entities_if(func);
+            if (!entities.empty()) {
+                std::string msg = "You see ";
+                for(auto e : entities) {
+                    auto name = e->get_component<Name>();
+                    if (name != nullptr) {
+                        msg += name->name; msg += " ";
+                    }
+                }
+                std::cout << msg;
+            } else {
+                std::cout << "You see nothing";
+            }
+            return false;
+            }
     });
 }
