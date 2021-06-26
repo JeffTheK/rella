753eeb4232477840d092c88b52409444c3173a4f
JeffTheK <jeff144@protonmail.com>
2021-06-26 16:27:37 +0300

    Add faction random() test

--- test/faction_test.cpp
+++ test/faction_test.cpp
@@ -1,9 +1,21 @@
 #include "../include/doctest.h"
 #include "../src/faction.hpp"
+#include <algorithm>
 
-TEST_CASE("") {
-
+TEST_CASE("random") {
+    auto results = std::vector<Faction::Type>{};
+    for (size_t i = 0; i < 1000; i++)
+    {
+        results.push_back(Faction::random().faction);
+    }
+    CHECK(std::find_if(results.begin(), results.end(), Faction::Type::enemy) != results.end());
+    CHECK(std::find_if(results.begin(), results.end(), Faction::Type::neutral) != results.end());
+    
+    // should not be present
+    CHECK(std::find_if(results.begin(), results.end(), Faction::Type::player) == results.end());
+    CHECK(std::find_if(results.begin(), results.end(), Faction::Type::null) == results.end());
 }
+
 TEST_CASE("load_from_json") {
     auto text = R"(
 {
